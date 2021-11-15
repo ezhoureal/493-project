04246717d6422d84f6d2b2467b1f7852bef323e9
Tianer Zhou <zireael@umich.edu>
2021-11-15 09:56:47 -0500

    voice app working

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-
 var resultView = new Vue({
   el: '#app',
   vuetify: new Vuetify(),
   data: {
+    records: [],
     activity: "",
     unit: "",
-    amount: 0,
+    amount: "",
     units: [
-      "miles", "kilometers", "minutes", "hours"
+      "MILES", "KILOMETERS", "MINUTES", "HOURS"
     ],
     form: true,
+  },
+
+  methods: {
+    amount_update(event) {
+      this.amount = event.target.value
+    },
+    activity_update(event) {
+      this.activity = event.target.value
+    },
+    unit_update(event) {
+      this.unit = event.target.value
+    },
+    submit() {
+      if (!this.unit || !this.records || !this.activity) {
+        return
+      }
+      this.records.push({"unit": this.unit, "amount": this.amount, "activity": this.activity})
+      this.unit = this.amount = this.activity = ""
+      this.form = false
+    }
   }
 })
   
